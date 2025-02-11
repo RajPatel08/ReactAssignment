2d22a56ef9dbbf9a3e6ff5a4b4835a29e0b4f58c
RajPatel08 <indop7939@gmail.com>
2025-02-11 12:48:30 +0530

    intial commit

---
+++ Assignment 1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <img className="image" src="Assignmment.jpg" alt="zerodha" width={"92%"} height={"92%"} />
+    </>
+  )
+}
+
+export default App
